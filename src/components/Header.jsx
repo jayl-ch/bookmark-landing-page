376f97b05f34dbd0ceed82f8c0bcc7f252f2b141
jayl-ch <jaychicogo328@gmail.com>
2026-01-08 00:32:27 +0800

    Fix navbar toggle menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,18 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Navbar from "./HeaderContent/Navbar";
 
 function Header() {
+  // navmenu
   const [isActive, setIsActive] = useState(false);
   const toggleIsActive = () => setIsActive((prev) => !prev);
+
+  useEffect(() => {
+    if (isActive) {
+      document.body.style.overflowY = "hidden";
+    } else {
+      document.body.style.overflowY = "auto";
+    }
+  }, [isActive]);
 
   return (
     <header>
