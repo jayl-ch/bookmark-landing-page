b854b0b2a50d8aaea04e1dc13613b119083c037e
jayl-ch <jaychicogo328@gmail.com>
2026-01-08 17:13:27 +0800

    feat: added extensions download section

--- src/components/HeaderContent/Login.jsx
+++ src/components/HeaderContent/Login.jsx
@@ -1,6 +1,6 @@
 const LoginBtn = () => (
   <button
-    className="w-full md:w-auto mt-8 text-[var(--neutral-grey-50)] tracking-widest text-xl font-semibold uppercase border-2 rounded-md border-[var(--neutral-grey-50)] md:text-sm md:mt-0 md:border-none py-3 md:px-8 md:bg-[var(--primary-red-400)]"
+    className="w-full md:w-auto cursor-pointer mt-8 text-[var(--neutral-grey-50)] tracking-widest text-xl font-semibold uppercase border-2 rounded-md border-[var(--neutral-grey-50)] md:text-sm md:mt-0 md:border-none py-3 md:px-8 md:bg-[var(--primary-red-400)]"
     type="button"
   >
     login
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 import Home from "./MainContent/Home";
 import Features from "./MainContent/Features";
+import Extensions from "./MainContent/Extensions";
 
 function Main() {
   // tab feature
@@ -11,6 +12,7 @@ function Main() {
     <main>
       <Home />
       <Features setIndex={setIndex} index={index} />
+      <Extensions />
     </main>
   );
 }
